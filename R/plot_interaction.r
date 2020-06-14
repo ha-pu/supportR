617837b7e31ba5fa20d4b14d56f41299dfa67366
ha-pu <66355167+ha-pu@users.noreply.github.com>
2020-06-14 09:37:41 +0200

    Bug fixing S3 generic/method consistency
    Change input parameter 'model' to 'input' in plot_interaction.lmerMod and plot_interaction.glmerMod

--- R/plot_interaction.r
+++ R/plot_interaction.r
@@ -48,7 +48,7 @@ plot_interaction.lm <- plot_interaction.glm <- function(input, iv, moderator, ci
 }
 
 # plot_interaction lmerMod and glmerMod method ----
-plot_interaction.lmerMod <- plot_interaction.glmerMod <- function(model, iv, moderator, ci = 0.9, ...) {
+plot_interaction.lmerMod <- plot_interaction.glmerMod <- function(input, iv, moderator, ci = 0.9, ...) {
   
   # run checks ----
   if (!is.character(iv) | !is.character(moderator)) stop("iv and moderator must be character variables!")
@@ -58,7 +58,7 @@ plot_interaction.lmerMod <- plot_interaction.glmerMod <- function(model, iv, mod
   }
   
   # draw plot ----
-  out <- interplot::interplot(model, var1 = iv, var2 = moderator, ci = ci, hist = TRUE) +
+  out <- interplot::interplot(input, var1 = iv, var2 = moderator, ci = ci, hist = TRUE) +
     ggplot2::labs(title = paste0("marginal effect ", iv), subtitle = paste0(ci * 100, "% confidence interval"), x = moderator, y = "marginal effect", caption = "Linear fixed effects model") +
     ggplot2::geom_hline(yintercept = 0)
   
