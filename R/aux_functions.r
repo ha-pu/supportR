ac378347e9922319158c675c824faba9b9d8e39c
ha-pu <66355167+ha-pu@users.noreply.github.com>
2020-06-30 17:06:32 +0200

    Add standardize_data

--- R/aux_functions.r
+++ R/aux_functions.r
@@ -1,4 +1,4 @@
-# .paste1 helper function ----
+# .paste1 [helper] ----
 .paste1 <- function(element1, element2, sep, collapse) {
   out <- c()
   for (i in element2) {
@@ -10,7 +10,7 @@
   return(out)
 }
 
-# create data function ----
+# create_data ----
 create_data <- function(n = 1000) {
   data_size <- n
   weights = runif(9, min = -2, max = 2)
@@ -52,13 +52,45 @@ create_data <- function(n = 1000) {
   return(data_value)
 }
 
+# clean_names ----
 clean_names <- function(data) {
 	
-	# run clean_names ----
+  # run clean_names ----
   if (requireNamespace("tibble", quietly = TRUE)) {
     out <- janitor::clean_names(data)
 	return(out)
   } else {
     warning("'clean_names' requires the 'jantitor' package!")
   }
+}
+
+# standardize_data ----
+standardize_data <- function(data, center = TRUE, scale = TRUE) {
+	
+	# run checks ----
+	if (!is.logical(center)) {
+		center <- TRUE
+		warning("center must be a logical value! Default for center is TRUE.")
+	}
+	if (!is.logical(scale)) {
+		scale <- TRUE
+		warning("scale must be a logical value! Default for scale is TRUE.")
+	}
+	
+	# run for "data.frame" %in% class(data) | "tbl_df" %in% class(data) ----
+	if ("data.frame" %in% class(data) | "tbl_df" %in% class(data)) {
+		# run standardization ----
+		numeric_vars <- unlist(lapply(data, is.numeric))
+		scaled_data <- scale(data[,numeric_vars], center = center, scale = scale)
+		out <- cbind.data.frame(scaled_data, data[,!numeric_vars])
+
+		# return output ----
+		if (requireNamespace("tibble", quietly = TRUE)) {
+			out <- tibble::as_tibble(out)
+		}
+
+		return(out)
+	} else {
+		warning("class(data) must be 'data.frame' or 'tbl_df'!")
+	}	
 }
