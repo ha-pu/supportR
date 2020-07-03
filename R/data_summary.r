1f75881d134b149ca492ba919e9d84fe7dd8f237
ha-pu <66355167+ha-pu@users.noreply.github.com>
2020-07-03 08:57:09 +0200

    Names for numeric "at"

--- R/data_summary.r
+++ R/data_summary.r
@@ -3,22 +3,33 @@ data_summary <- function(input, ...) UseMethod("data_summary", input)
 
 # data_summary data.frame method ----
 data_summary.data.frame <- function(input, at, show = TRUE, ...) {
-  input <- as.data.frame(input)
-  out <- lapply(at, function(x) .get_summary(input[,x]))
-  out <- cbind.data.frame(out)
-  colnames(out) <- at
-  out$Statistic <- stat
-  
-  out <- tidyr::gather(out, -Statistic, key = "Variable", value = "value")
-  out <- tidyr::spread(out, key = Statistic, value = value)
-  
-  out <- out[c("Variable", stat)]
-  out[,stat[-1]] <- sapply(out[,stat[-1]], as.numeric)
-  
-  if (show) {
-    .print_data_summary(input = out)
+  if (is.numeric(at)) {
+    if (is.double(at)) {
+      warning("'at' of class 'double' is coerced to class 'integer'!")
+      at <- as.integer(at)
+    }
+    at <- names(input)[at]
+  }
+  if (is.character(at)) {
+    input <- as.data.frame(input)
+    out <- lapply(at, function(x) .get_summary(input[,x]))
+    out <- cbind.data.frame(out)
+    colnames(out) <- at
+    out$Statistic <- stat
+
+    out <- tidyr::gather(out, -Statistic, key = "Variable", value = "value")
+    out <- tidyr::spread(out, key = Statistic, value = value)
+
+    out <- out[c("Variable", stat)]
+    out[,stat[-1]] <- sapply(out[,stat[-1]], as.numeric)
+
+    if (show) {
+      .print_data_summary(input = out)
+    } else {
+      return(out)
+    }
   } else {
-    return(out)
+   warning("'at' must be class 'character' or 'numeric'!")
   }
 }
 
@@ -32,7 +43,7 @@ data_summary.grouped_df <- function(input, at, show = TRUE, ...) {
   out$Cluster <- paste(names(attr(input,"groups"))[1], as.data.frame(attr(input,"groups"))[,1], sep = ": ")
   out <- out[,c("Variable", "Cluster", stat)]
   out <- out[order(out$Variable, out$Cluster),]
-  
+
   if (show) {
     .print_data_summary(input = out)
   } else {
@@ -44,7 +55,7 @@ data_summary.grouped_df <- function(input, at, show = TRUE, ...) {
 data_summary.numeric <- data_summary.logical <- data_summary.character <- data_summary.factor <- function(input, show = TRUE, ...) {
   input <- as.data.frame(input)
   out <- data_summary(input = input, at = "input", show = FALSE)
-  
+
   if (show) {
     .print_data_summary(input = out)
   } else {
