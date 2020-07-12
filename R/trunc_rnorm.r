c426ea0497680e755398cd995644b3ddb0428c48
ha-pu <66355167+ha-pu@users.noreply.github.com>
2020-07-12 15:37:05 +0200

    Change defaults

--- R/trunc_rnorm.r
+++ R/trunc_rnorm.r
@@ -1,5 +1,5 @@
 # trunc_rnorm ----
-trunc_rnorm <- function(n, mean = 0, sd = 1, lwr = -1, upr = 1, nnorm = n * 1.1) {
+trunc_rnorm <- function(n, mean = 0, sd = 1, lwr = -Inf, upr = Inf, nnorm = n) {
   message(nnorm)
   samp <- rnorm(n = nnorm, mean = mean, sd = sd)
   samp <- samp[samp >= lwr & samp <= upr]
