e34d4bef1a061954448bdbccb47c18ac1d476a43
ha-pu <66355167+ha-pu@users.noreply.github.com>
2020-09-08 09:56:33 +0200

    Adapt to roxgen2

--- R/clean_names.r
+++ R/clean_names.r
@@ -1,6 +1,24 @@
+#' @title
+#' Clean names of an object.
+#'
+#' @description
+#' The function imports the \code{clean_names} function from the \code{janitor} package.
+#'
+#' @section Warning:
+#' The function requires that the \code{janitor} is installed!
+#'
+#' @seealso \code{\link[janitor]{clean_names}}
+#'
+#' @param data Object, usually \code{data.frame}, with column names to clean.
+#' @return The input object \code{data} with clean column names.
+#' @examples
+#' names(data) <- gsub("_", ".", names(data))
+#' clean_names(data)
+#' @importFrom janitor clean_names
+
 # clean_names ----
 clean_names <- function(data) {
-	
+
   # run clean_names ----
   if (requireNamespace("janitor", quietly = TRUE)) {
     out <- janitor::clean_names(data)
